053b7b0df2a69752fb91d0c2b93216a7cee83c32
Vladislav Rishe <vlados.v10.0@gmail.com>
2016-01-02 02:18:53 +0300

    Experimental dynamic frame texture output is added; Current implementation simply flashes a single display block @ 1Hz.

--- src/main.cpp
+++ src/main.cpp
@@ -4,29 +4,60 @@
 #include "stdafx.h"
 
 #include "main.h"
+#include "chip8\Chip8Interpreter.h"
 
 typedef struct tagChip8DisplayExtra {
 	HDC		hDC;
 	HGLRC	hGLRC;
 
-	GLuint  screenVBO;
+	GLuint screenVBO;
+	GLuint screenEBO;
+	GLuint screenTex;
+	GLuint screenShaderV;
+	GLuint screenShaderF;
+	GLuint screenShaderPrg;
 } Chip8DisplayExtra;
 
 typedef struct tagPoint2D {
 	GLfloat x;
 	GLfloat y;
+
+	GLfloat u;
+	GLfloat v;
 } Point2D;
 
+static chip8::Interpreter::Frame testFrame;
+
+// Shader sources
+static const GLchar* vertexSource =
+	"#version 150 compatibility\n"
+	"out vec2 frameCoord;"
+	"\n"
+	"void main() {"
+	"	frameCoord = vec2(gl_MultiTexCoord0[0], 1 - gl_MultiTexCoord0[1]);"
+	"   gl_Position = ftransform();"
+	"}";
+static const GLchar* fragmentSource =
+	"#version 150 compatibility\n"
+	"uniform sampler2D frame;"
+	"in vec2 frameCoord;"
+	"\n"
+	"void main() {"
+	"	gl_FragColor = texture2D(frame, frameCoord).rrra;"
+	"}";
 
 void PrintOpenGLVersion();
+void PrintOpenGLShaderLog(GLuint shaderId);
 
 #ifdef _DEBUG
-#define PRINT_OPENGL_VERSIOND() PrintOpenGLVersion()
+#define PRINT_OPENGL_VERSIOND()				PrintOpenGLVersion()
+#define PRINT_OPENGL_SHADER_LOG(shaderId)	PrintOpenGLShaderLog(shaderId);
 #else
 #define PRINT_OPENGL_VERSIOND()
+#define PRINT_OPENGL_SHADER_LOG(shaderId)
 #endif // _DEBUG
 
-static void CreateSceneContext(HDC hDC, Chip8DisplayExtra *extra) {
+static void CreateGLContext(HDC hDC, Chip8DisplayExtra *extra) {
 	HGLRC hGLRC;
 	{
 		PIXELFORMATDESCRIPTOR pfd = {
@@ -58,39 +89,119 @@ static void CreateSceneContext(HDC hDC, Chip8DisplayExtra *extra) {
 	PRINT_OPENGL_VERSIOND();
 	glewInit();
 
+	glDisable(GL_DEPTH_TEST);
 	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
 	extra->hDC = hDC;
 	extra->hGLRC = hGLRC;
 }
 
-static void DestroySceneContext(Chip8DisplayExtra *extra) {
+static void DestroyGLContext(Chip8DisplayExtra *extra) {
 	wglMakeCurrent(extra->hDC, NULL);
 	wglDeleteContext(extra->hGLRC);
 }
 
-static void CreateSceneVBO(Chip8DisplayExtra *extra, double l, double t, double r, double b) {
+static void CreateGLScene(Chip8DisplayExtra *extra, double l, double t, double r, double b) {
 	Point2D points[] = {
-		{ l, b },
-		{ r, t },
-		{ l, t },
-		{ l, b },
-		{ r, b },
-		{ r, t }
+		{ l, t, 0, 1 },
+		{ r, t, 1, 1 },
+		{ r, b, 1, 0 },
+		{ l, b, 0, 0 },
 	};
-	GLuint bufferId;
+	GLuint element[] = {
+		0, 1, 2,
+		2, 3, 0
+	};
+	GLuint idVBO;
+	GLuint idEBO;
 
-	glGenBuffers(1, &bufferId);
-	glBindBuffer(GL_ARRAY_BUFFER, bufferId);
+	glGenBuffers(1, &idVBO);
+	glGenBuffers(1, &idEBO);
+	glBindBuffer(GL_ARRAY_BUFFER, idVBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, idEBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	
-	extra->screenVBO = bufferId;
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(element), element, GL_STATIC_DRAW);
+
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+	glVertexPointer(2, GL_FLOAT, sizeof(Point2D), NULL);
+	glTexCoordPointer(2, GL_FLOAT, sizeof(Point2D), (void*)(2 * sizeof(GLfloat)));
+
+	GLuint idTex;
+	GLuint idShaderV, idShaderF, idShaderPrg;
+
+	glGenTextures(1, &idTex);
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, idTex);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_R8, chip8::Interpreter::FRAME_WIDTH, 
+		chip8::Interpreter::FRAME_HEIGHT, 0, GL_RED, GL_UNSIGNED_BYTE, testFrame);
+
+	idShaderV = glCreateShader(GL_VERTEX_SHADER);
+	idShaderF = glCreateShader(GL_FRAGMENT_SHADER);
+
+	glShaderSource(idShaderV, 1, &vertexSource,   NULL); glCompileShader(idShaderV); PRINT_OPENGL_SHADER_LOG(idShaderV);
+	glShaderSource(idShaderF, 1, &fragmentSource, NULL); glCompileShader(idShaderF); PRINT_OPENGL_SHADER_LOG(idShaderF);
+
+	idShaderPrg = glCreateProgram();
+	glAttachShader(idShaderPrg, idShaderV);
+	glAttachShader(idShaderPrg, idShaderF);
+	glLinkProgram(idShaderPrg);
+
+	int linked;
+	glGetProgramiv(idShaderPrg, GL_LINK_STATUS, &linked);
+	assert(linked);
+	{
+		GLint uniformFrame = glGetUniformLocation(idShaderPrg, "frame");
+
+		glUniform1i(uniformFrame, 0);
+	}
+	glUseProgram(idShaderPrg);
+
+	extra->screenVBO = idVBO;
+	extra->screenEBO = idEBO;
+	extra->screenTex = idTex;
+	extra->screenShaderV = idShaderV;
+	extra->screenShaderF = idShaderF;
+	extra->screenShaderPrg = idShaderPrg;
 }
 
-static void DestroySceneVBO(Chip8DisplayExtra *extra) {
+static void DestroyGLScene(Chip8DisplayExtra *extra) {
+	glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+	glDisableClientState(GL_VERTEX_ARRAY);
+
+	glUseProgram(0);
+	glDeleteProgram(extra->screenShaderPrg);
+	glDeleteShader(extra->screenShaderF);
+	glDeleteShader(extra->screenShaderV);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glDeleteBuffers(1, &extra->screenEBO);
 	glDeleteBuffers(1, &extra->screenVBO);
+}
+
+static DWORD time;
+
+static void CALLBACK TestTickHandler() {
+	testFrame[16] ^= 0xFF;
+
+	glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, chip8::Interpreter::FRAME_WIDTH,
+		chip8::Interpreter::FRAME_HEIGHT, GL_RED, GL_UNSIGNED_BYTE, testFrame);
+}
+
+static void DrawGLScene(Chip8DisplayExtra *extra) {
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	DWORD currentTime = GetTickCount();
+
+	if (currentTime - time > 500) {
+		time = currentTime;
+
+		TestTickHandler();		
+	}
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 }
 
 
@@ -101,8 +212,8 @@ static BOOL Chip8DisplayCreate(HWND hWnd, LPCREATESTRUCT lpCreateStruct) {
 
 	auto extra = new Chip8DisplayExtra;
 	{
-		CreateSceneContext(GetDC(hWnd), extra);
-		CreateSceneVBO(extra, 0, 0, 0, 0);		// Not sure we need this.
+		CreateGLContext(GetDC(hWnd), extra);
+		CreateGLScene(extra, 0, 0, 0, 0);				// Not sure we need this.
 	}
 	ASSIGN_DISPLAY_EXTRA(hWnd, extra);
 
@@ -112,8 +223,8 @@ static BOOL Chip8DisplayCreate(HWND hWnd, LPCREATESTRUCT lpCreateStruct) {
 static VOID Chip8DisplayDestroy(HWND hWnd) {
 	auto extra = OBTAIN_DISPLAY_EXTRA(hWnd);
 	{
-		DestroySceneVBO(extra);
-		DestroySceneContext(extra);
+		DestroyGLScene(extra);
+		DestroyGLContext(extra);
 
 		ReleaseDC(hWnd, extra->hDC);
 	}
@@ -124,17 +235,9 @@ static VOID Chip8DisplayDestroy(HWND hWnd) {
 
 static VOID Chip8DisplayPaint(HWND hWnd) {
 	auto extra = OBTAIN_DISPLAY_EXTRA(hWnd);
-
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	{
-		glEnableClientState(GL_VERTEX_ARRAY);
-		glBindBuffer(GL_ARRAY_BUFFER, extra->screenVBO);
-		glVertexPointer(2, GL_FLOAT, 0, NULL);
-		glDrawArrays(GL_TRIANGLES, 0, 6);
-		glDisableClientState(GL_VERTEX_ARRAY);
+		DrawGLScene(extra);
 	}
-	glFlush();
-
 	SwapBuffers(extra->hDC);
 }
 
@@ -144,30 +247,26 @@ static VOID Chip8DisplaySize(HWND hWnd, UINT state, int cx, int cy) {
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 
-	double l, t, r, b;
+	double l = -1.0, t = 1.0, r = 1.0, b = -1.0;
 
 	if (cx > cy) {
 		auto aspect = cx / (double)cy;
 
-		l = -1.0 * aspect;
-		r =  1.0 * aspect;
-		b = -1.0;
-		t =  1.0;
+		l *= aspect;
+		r *= aspect;
 	}
 	else if (cy > cx) {
 		auto aspect = cy / (double)cx;
 
-		l = -1.0;
-		r =  1.0;
-		b = -1.0 * aspect;
-		t =  1.0 * aspect;
+		b *= aspect;
+		t *= aspect;
 	}
 	gluOrtho2D(l, r, b, t);
 
 	auto extra = OBTAIN_DISPLAY_EXTRA(hWnd);
 
-	DestroySceneVBO(extra);
-	CreateSceneVBO(extra, l, t, r, b);
+	DestroyGLScene(extra);
+	CreateGLScene(extra, l, t, r, b);
 }
 
 #ifdef _DEBUG
@@ -177,10 +276,10 @@ static void PrintOpenGLVersion() {
 		auto glVer = std::string(reinterpret_cast<LPCCH>(glGetString(GL_VERSION)));
 
 #ifdef UNICODE
-		auto size = MultiByteToWideChar(CP_UTF8, 0, glVer.c_str(), glVer.size(), NULL, 0);
+		auto size = MultiByteToWideChar(CP_ACP, 0, glVer.c_str(), glVer.size(), NULL, 0);
 
 		glVersionText = new TCHAR[size];
-		MultiByteToWideChar(CP_UTF8, 0, glVer.c_str(), glVer.size(), glVersionText, size);
+		MultiByteToWideChar(CP_ACP, 0, glVer.c_str(), glVer.size(), glVersionText, size);
 #else
 		auto size = glVer.size();
 		glVersionText = new TCHAR[size];
@@ -190,8 +289,53 @@ static void PrintOpenGLVersion() {
 	}
 	OutputDebugString(_T("OpenGL version: "));
 	OutputDebugString(glVersionText);
+	OutputDebugString(_T("\n\n"));
 
 	delete[] glVersionText;
+}
+
+void PrintOpenGLShaderLog(GLuint shaderId) {
+	int infologLen = 0;
+
+	glGetShaderiv(shaderId, GL_INFO_LOG_LENGTH, &infologLen);
+
+	if (infologLen > 0) {
+		char *infoLog = new GLchar[infologLen];
+
+		if (infoLog == NULL)
+		{
+			OutputDebugString(_T("ERROR: Could not allocate InfoLog buffer\n"));
+
+			exit(1);
+		}
+		else {
+			int charsWritten;
+			glGetShaderInfoLog(shaderId, infologLen, &charsWritten, infoLog);
+
+			LPTSTR shaderLogText;
+			{
+				int size;
+
+#ifdef UNICODE
+				size = MultiByteToWideChar(CP_ACP, 0, infoLog, charsWritten, NULL, 0);
+
+				shaderLogText = new TCHAR[size];
+				MultiByteToWideChar(CP_ACP, 0, infoLog, charsWritten, shaderLogText, size);
+#else
+				size = charsWritten;
+				shaderLogText = new TCHAR[size];
+
+				memcpy_s(shaderLogText, size, infoLog, size);
+#endif // UNICODE
+			}			
+			OutputDebugString(_T("Shader info log:"));
+			OutputDebugString(shaderLogText);
+			OutputDebugString(_T("\n\n"));
+
+			delete[] shaderLogText;
+		}
+		delete[] infoLog;
+	}
 }
 #endif
 
